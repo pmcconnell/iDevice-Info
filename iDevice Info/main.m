98f1320e6df1af51d8d09323c57d4daf15c50a9a
Patrick McConnell <patrick@dogboystudios.com>
2013-01-09 09:25:47 -0500

    Initial Commit

---
+++ iDevice Info/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iDevice Info
+//
+//  Created by Patrick Mcconnell on 1/9/13.
+//  Copyright (c) 2013 Dogboy Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DBSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([DBSAppDelegate class]));
+  }
+}
